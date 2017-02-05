4dbcf014c1429796b1fd542f6a770e90377361fa
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-02-05 10:09:07 +0100

    #12 replace use of callsite with standard Error object to fix issues with babel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 var AssertionError = require('assert').AssertionError
   , callsite = require('callsite')
-  , fs = require('fs')
+  , fs = require('fs');
 
 /**
  * Expose `assert`.
@@ -21,18 +21,24 @@ module.exports = process.env.NO_ASSERT
 function assert(expr) {
   if (expr) return;
 
-  var stack = callsite();
-  var call = stack[1];
-  var file = call.getFileName();
-  var lineno = call.getLineNumber();
-  var src = fs.readFileSync(file, 'utf8');
-  var line = src.split('\n')[lineno-1];
-  var src = line.match(/assert\((.*)\)/)[1];
-
-  var err = new AssertionError({
-    message: src,
-    stackStartFunction: stack[0].getFunction()
-  });
-
-  throw err;
+    var a = new Error();
+    // 0 => Error
+    // 1 => at assert
+    // 2 =>  at Object.<anonymous> (/project/myproject/test/test-babel.js:15:1)', <= where the assert was raised !
+    // .....
+    //
+    var errorline = a.stack.split('\n')[2];
+    var m =  errorline.match(/at (.*)\((.*):([0-9]*):([0-9]*)\)/);
+    var func =  m[1]; // Object.<anonymous> ( not very useful)
+    var file =  m[2]; // filename
+    var lineno = parseInt(m[3]);
+    var fullsource = fs.readFileSync(file, 'utf8');
+    var line = fullsource.split('\n')[lineno-1];
+    var src = line.match(/.*assert\((.*)\)/)[1];
+    var err = new AssertionError({
+        message: src + "\n ",
+        stackStartFunction: assert
+    });
+    throw err;
 }
+
