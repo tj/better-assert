99e16ca2563995e735f9087ae570f233113d5f37
Dario Cangialosi, Coder <dario.cangialosi@gmail.com>
2018-08-20 01:06:19 +0200

    new way, with ()=> syntax. replace 'assert(' with 'assert(()=>' to upgrade.

--- example.js
+++ example.js
@@ -5,12 +5,12 @@ test()
 
 function test() {
   var user = { name: 'tobi'
-	 // , age: 18
+	  // , age: 18
 	   }
   
-  //assert(false)
-  assert('tobi' == user.name, "'tobi' == user.name")
-  assert('number' == typeof user.age, "'number' == typeof user.age")
+  //assert(()=>false)
+  assert(()=>'tobi' == user.name)
+  assert(()=>'number' == typeof user.age)
   
   console.log('test ended.')
 }
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-module.exports = function assert(test,
+module.exports = assert2
+
+function assert2(test,
 message='<custom message not provided.'+
 ' see the stacktrace to see the origin of this error.>'){
 	if(test!==true)
 		throw new Error('Assertion is false: '+message)
 }
+
+module.exports = assert
+
+function assert(test){
+	if(test()!==true)
+		throw new Error('Assertion false: '+test.toString().split('=>')[1])
+}
+
+var name='rich'
+assert(()=>'rich'==name)
